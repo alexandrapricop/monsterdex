bcd1a996c0ba715a984b82197fbada8c454288ea
Alexandra Pricop <alexandrag_pricop@yahoo.com>
2019-11-30 14:08:38 +0200

    Updates

---
+++ precache-manifest.aeeaba675fb7d65043cc4a5d233201f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a869a39a23ea56e7cfefe1619283715a",
+    "url": "/monsterdex/index.html"
+  },
+  {
+    "revision": "ad1c92ef048f7db870f9",
+    "url": "/monsterdex/static/css/main.4b6a56b0.chunk.css"
+  },
+  {
+    "revision": "71acf90169e695ae9fc3",
+    "url": "/monsterdex/static/js/2.f43442f1.chunk.js"
+  },
+  {
+    "revision": "ad1c92ef048f7db870f9",
+    "url": "/monsterdex/static/js/main.4ce07978.chunk.js"
+  },
+  {
+    "revision": "2ea3d9b5b13d2338be3b",
+    "url": "/monsterdex/static/js/runtime-main.3cb67d50.js"
+  }
+]);
